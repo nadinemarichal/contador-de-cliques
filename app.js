ab508ca3d9922b748f8041ca684e6c1f1fe4599d
nadinemarichal <rodrigues.nnm@gmail.com>
2025-12-10 17:06:14 -0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Contador from './Contador'; // Certifique-se de que o caminho está correto
+import './App.css'; // Opcional: para estilos
+
+function App() {
+  return (
+    <div className="App">
+      <Contador />
+    </div>
+  );
+}
+
+export default App;
